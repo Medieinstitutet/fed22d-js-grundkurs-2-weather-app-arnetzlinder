6363d3c6958ae469b2f24e0ddea35c141711b465
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2022-12-15 09:11:53 +0100

    ts

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
 /* eslint linebreak-style: ["error", "windows"] */
 var electricityPrice = document.querySelector('.electricityPrice');
 // let electricityAreaChoice: string;
 var electricityArea;
 var electricityPrices = [];
+var date = new Date();
+var time;
+function displayElectricityPrice() {
+    electricityPrice.innerHTML = '';
+    electricityPrice.innerHTML += "\n  <h2> Elpris just nu: ".concat(electricityPrices[2][date.getHours()], " \u00F6re/kWh</h2>\n  ");
+}
 function getElectricityAreaPrices(area) {
     // const response = fetch('https://entsoe-cache.plsh.se/SE3.json');
     // electricityPrices = response.json() as string[];
@@ -15,14 +17,42 @@ function getElectricityAreaPrices(area) {
         .then(function (json) {
         console.table(json);
         electricityPrices = json;
+        // return electricityPrices[0][20];
+        console.log(electricityPrices[0][9]);
+        displayElectricityPrice();
     })["catch"](function (err) {
         console.error('Error fetching electricity prices:', err);
     });
 }
-// Kolla upp hur du gör innerHTML i typescript
-function displayElectricityPrice() {
-    electricityPrice.innerHTML = '';
-    electricityPrice.innerHTML += "\n  <h2> Elpris just nu: ".concat(electricityPrices[0]);
-    .2;
+function chooseElectricityArea(e) {
+    /* Här lägger jag in om kunden väljer område ett visas elprisområde ett osv */
+    var element = e.currentTarget;
+    switch (element.value) {
+        case 'electricityArea1':
+            getElectricityAreaPrices('SE1');
+            break;
+        case 'electricityArea2':
+            getElectricityAreaPrices('SE2');
+            break;
+        case 'electricityArea3':
+            getElectricityAreaPrices('SE3');
+            break;
+        case 'electricityArea4':
+            getElectricityAreaPrices('SE4');
+            break;
+        default:
+    }
+    console.log(chooseElectricityArea);
 }
-/h2>(__makeTemplateObject([";\n}\n\nfunction chooseElectricityArea(e: Event) {\n  /* H\u00E4r l\u00E4gger jag in om kunden v\u00E4ljer omr\u00E5de ett visas elprisomr\u00E5de ett osv */\n  const element = e.currentTarget as HTMLSelectElement;\n  switch (element.value) {\n    case 'electricityArea1':\n      electricityPrice = getElectricityAreaPrices('SE1');\n      break;\n    case 'electricityArea2':\n      electricityPrice = getElectricityAreaPrices('SE2');\n      break;\n    case 'electricityArea3':\n      electricityPrice = getElectricityAreaPrices('SE3');\n      break;\n    case 'electricityArea4':\n      electricityPrice = getElectricityAreaPrices('SE4');\n      break;\n    default:\n     // displayElectricityPrice();\n  }\n\n  console.log(chooseElectricityArea);\n}\n\nfunction initFields() {\n// Function to declare fields\n  electricityArea = document.querySelector('.chooseElectricityArea') as Element;\n  electricityArea.addEventListener('change', chooseElectricityArea);\n}\n\ninitFields();\n\n/*\nif(elpriset \u00E4r h\u00F6gre \u00E4n medel) {\n  // Time to freeze!\n  let e = document.querySelector(\".electricityPriceBackground\");\n  e.style.backgroundImage = \"url(../../public/photos/frost.jpg)\";\n} */\n"], [";\n}\n\nfunction chooseElectricityArea(e: Event) {\n  /* H\u00E4r l\u00E4gger jag in om kunden v\u00E4ljer omr\u00E5de ett visas elprisomr\u00E5de ett osv */\n  const element = e.currentTarget as HTMLSelectElement;\n  switch (element.value) {\n    case 'electricityArea1':\n      electricityPrice = getElectricityAreaPrices('SE1');\n      break;\n    case 'electricityArea2':\n      electricityPrice = getElectricityAreaPrices('SE2');\n      break;\n    case 'electricityArea3':\n      electricityPrice = getElectricityAreaPrices('SE3');\n      break;\n    case 'electricityArea4':\n      electricityPrice = getElectricityAreaPrices('SE4');\n      break;\n    default:\n     // displayElectricityPrice();\n  }\n\n  console.log(chooseElectricityArea);\n}\n\nfunction initFields() {\n// Function to declare fields\n  electricityArea = document.querySelector('.chooseElectricityArea') as Element;\n  electricityArea.addEventListener('change', chooseElectricityArea);\n}\n\ninitFields();\n\n/*\nif(elpriset \u00E4r h\u00F6gre \u00E4n medel) {\n  // Time to freeze!\n  let e = document.querySelector(\".electricityPriceBackground\");\n  e.style.backgroundImage = \"url(../../public/photos/frost.jpg)\";\n} */\n"]));
+function initFields() {
+    // Function to declare fields
+    electricityArea = document.querySelector('.chooseElectricityArea');
+    electricityArea.addEventListener('change', chooseElectricityArea);
+}
+initFields();
+/*
+if(elpriset är högre än medel) {
+  // Time to freeze!
+  let e = document.querySelector(".electricityPriceBackground");
+  e.style.backgroundImage = "url(../../public/photos/frost.jpg)";
+} */
